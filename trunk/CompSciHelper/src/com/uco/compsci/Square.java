4000d6be00175b75eef2daa4b4371541985a7a77
Jess.Louderback@gmail.com <Jess.Louderback@gmail.com@61cb076c-e8b7-d1a0-78c0-72fdff911ace>
2011-10-09 18:23:54 +0000

    Fixed

--- trunk/CompSciHelper/src/com/uco/compsci/Square.java
+++ trunk/CompSciHelper/src/com/uco/compsci/Square.java
@@ -22,11 +22,18 @@ public class Square extends Shape {
 		setY(yloc);
 		setNumb(n);
 	}
+	public Square(int xloc, int yloc, int n, int color) {
+		x = xloc;
+		setY(yloc);
+		setNumb(n);
+		super.setColor(color);
+	}
 
 	public Square(Shape s) {
 		x = s.x;
 		setY(s.getY());
 		setNumb(s.getNumb());
+		super.setColor(s.getColor());
 	}
 
 	public Square(Shape prev, int n) {
